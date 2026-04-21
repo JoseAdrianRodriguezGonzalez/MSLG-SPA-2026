bd413ed81c4bb04ce16548497e5c6680fa29236e
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2026-04-21 17:03:13 -0600

    se añade la limpieza de datos

--- ingest/pipeline.py
+++ ingest/pipeline.py
@@ -1,4 +1,7 @@
 from ingest.read import read
+from ingest.preprocessing import pipeline_mslg
 def pipeline(src):
     df=read(src)
-    print(df)
+    df["MSLG"]=df["MSLG"].apply(pipeline_mslg)
+    print(df["MSLG"])
+    df.to_csv("data/cleaned.csv",index=False)
